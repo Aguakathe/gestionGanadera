0d6634065926258790c58e003a12cb2e0df55880
Aguakathe <sktorres6@gmail.com>
2025-04-09 18:22:24 -0500

    Se quitó el Metodo para Activar,Desactivar y verificar existencias, se agregó el metodo eliminar

--- src/datos/interfaces/CrudSimpleInterface.java
+++ src/datos/interfaces/CrudSimpleInterface.java
@@ -5,21 +5,17 @@ import java.util.List;
 
 
 public interface CrudSimpleInterface <T> { //T SIGINIFICA UN OBJETO QUE SE LE VA A ESTAR PASANDO
-    //metodos obligarorios, metodo para listar 
+    //Metodos obligatorios
+    
+    //Metodo para listar 
     public List<T> listar(String texto);
-    //metodo para insertar un nuevo objeto
+    //Metodo para insertar un nuevo objeto
     public boolean insertar(T obj);
-    //metodo para actualizar un objero del sistema
+    //Metodo para actualizar un objero del sistema
     public boolean actualizar(T obj);
-    
-    //metod para desactivar el objeto existente
-    public boolean desactivar(int id);
-    
-    public boolean activar(int id);
-    //metdo para contar el total de elementos registrados
+    //Metodo para eliminar por ID
+    public boolean eliminar(int id);
+    //Método para contar el total de elementos registrados
     public int total();
-    
-    //metodo de verificacion si la categoria existe o no
-    public boolean existe(String texto);
     
 }
